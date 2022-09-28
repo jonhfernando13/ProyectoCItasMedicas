b53582b70604b3029146537816246699cad65dfd
jonhfernando13@ <jonhfernando13@gmail.com>
2022-09-28 15:36:31 -0500

    direccion

--- src/main/java/com/example/demo/controller/CitaController.java
+++ src/main/java/com/example/demo/controller/CitaController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.Optional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,7 +29,7 @@ public class CitaController {
 
     }
 
-    @PostMapping(value = "/guardar",produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/guardar")
     public CitaModelo guardarcita(@RequestBody CitaModelo cita) {
 
         return this.citaService.guardarcita(cita);
